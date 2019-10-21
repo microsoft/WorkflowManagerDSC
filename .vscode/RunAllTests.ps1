ff4e60f3772d4c7ee663004e9d6c43ba77fdf772
Priviliged Ilian <pilian.nilsson@visity.local>
2019-10-22 01:39:16 +0200

    Workaround for WFCU5 reference issue

--- .vscode/RunAllTests.ps1
+++ .vscode/RunAllTests.ps1
@@ -1,6 +1,6 @@
-Import-Module (Join-Path $PSScriptRoot "..\Tests\Unit\OfficeOnlineServerDsc.TestHarness.psm1"  -Resolve) -Force
+Import-Module (Join-Path $PSScriptRoot "..\Tests\Unit\WorkflowManagerDsc.TestHarness.psm1"  -Resolve) -Force
 
-$DscTestsPath = Join-Path $PSScriptRoot "..\Modules\OfficeOnlineServerDsc\DscResource.Tests" -Resolve
+$DscTestsPath = Join-Path $PSScriptRoot "..\Modules\WorkflowManagerDsc\DscResource.Tests" -Resolve
 if ((Test-Path $DscTestsPath) -eq $false) {
     Write-Warning "Unable to locate DscResource.Tests repo at '$DscTestsPath', common DSC resource tests will not be executed"
     Invoke-OosDscUnitTestSuite -CalculateTestCoverage $false
--- .vscode/RunGlobalTestsOnly.ps1
+++ .vscode/RunGlobalTestsOnly.ps1
@@ -1,11 +1,11 @@
-Import-Module (Join-Path $PSScriptRoot "..\Tests\Unit\OfficeOnlineServerDsc.TestHarness.psm1")
+Import-Module (Join-Path $PSScriptRoot "..\Tests\Unit\WorkflowManagerDsc.TestHarness.psm1")
 
-$DscTestsPath = Join-Path $PSScriptRoot "..\Modules\OfficeOnlineServerDsc\DscResource.Tests"
+$DscTestsPath = Join-Path $PSScriptRoot "..\Modules\WorkflowManagerDsc\DscResource.Tests"
 if ((Test-Path $DscTestsPath) -eq $false) {
     throw "Unable to locate DscResource.Tests repo at '$DscTestsPath', common DSC resource tests will not be executed"
 }
-Import-Module (Join-Path $PSScriptRoot "..\Modules\OfficeOnlineServerDsc\OfficeOnlineServerDsc.Tests\TestHelper.psm1")
+Import-Module (Join-Path $PSScriptRoot "..\Modules\WorkflowManagerDsc\WorkflowManagerDsc.Tests\TestHelper.psm1")
 
-Set-Location -Path (Join-Path $PSScriptRoot "..\Modules\OfficeOnlineServerDsc\DscResource.Tests")
+Set-Location -Path (Join-Path $PSScriptRoot "..\Modules\WorkflowManagerDsc\DscResource.Tests")
 
-Invoke-Pester 
+Invoke-Pester
